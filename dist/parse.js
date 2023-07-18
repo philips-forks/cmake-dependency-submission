778774ed131770f7cec377d5f0f0194fab73d9c9
Ron Jaegers <ron.jaegers@philips.com>
2023-07-18 09:22:53 +0000

    build: update dist folder

--- dist/parse.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseProject = void 0;
-function parseProject(projectRoot) {
-    console.log("Hello World! ${projectRoot}");
-    return 4;
-}
-exports.parseProject = parseProject;
